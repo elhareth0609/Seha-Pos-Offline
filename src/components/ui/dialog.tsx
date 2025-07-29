ec613311cf125b7e9abba46d5585916e3346a812
elhareth0609 <elhareh0609@gmail.com>
2025-07-29 12:49:07 +0100

    fix dialog footer responsive layout

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -73,7 +73,8 @@ const DialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      "flex flex-col-reverse md:flex-row-reverse md:justify-start sm:flex-row md:space-x-2 sm:space-x-2",
+      // "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
       className
     )}
     {...props}
